b3f7cb4bc8f1743629005e348f02d89323e04cb6
cesarhj19 <cesarhj19@gmail.com>
2019-09-30 19:39:48 -0700

    MVP Day Two Completed

--- src/components/PotdCard.js
+++ src/components/PotdCard.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledCardContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  background: lightgrey;
+  border-radius: 15px;
+`;
+
+const StyledCardHeading = styled.h1`
+  font-size: 50px;
+  margin-bottom: 5px;
+`;
+
+const StyledP = styled.p`
+  width: 80%;
+  text-align: center;
+  margin: 0 auto;
+  padding: 20px 0;
+`;
 
 function PotdCard(props) {
   return (
-    <div className="card-container" key={props.id}>
-      <h1>{props.title}</h1>
+    <StyledCardContainer>
+      <StyledCardHeading>{props.title}</StyledCardHeading>
 
       <div className="date">
-        <p>{props.date}</p>
+        <StyledP>{props.date}</StyledP>
       </div>
 
       <div className="img-container">
         <img className="img" alt="space" src={props.picture}></img>
       </div>
-      <p>{props.description}</p>
-    </div>
+      <StyledP>{props.description}</StyledP>
+    </StyledCardContainer>
   );
 }
 
